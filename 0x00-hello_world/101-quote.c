73dfa558b15bbee72bfa8da55f4d78a4f025abc1
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-02 15:46:12 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i;
+	int size = sizeof(quote);
+
+	for (i = 0; i < size; i++)
+		putchar(quote[i]);
+	putchar('\n');
+	return (1);
+}

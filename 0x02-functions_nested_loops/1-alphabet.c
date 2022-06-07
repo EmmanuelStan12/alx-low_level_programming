e551c2a7b8ea2f8d26a7add12678cd3a5be6e73c
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-07 10:51:06 +0100

    10x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,6 +13,7 @@ void print_alphabet(void)
 	while (c != 123)
 	{
 		putchar(c);
+		c++;
 	}
 	putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ * prints all the alphabets in small letters ten times
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	c = 'a';
+	while (c != 123)
+	{
+		putchar(c);
+		c++;
+	}
+}

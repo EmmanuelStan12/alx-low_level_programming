947ef908e436d16bc6c4bb9c186b91a22fd8332f
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-07 18:00:41 +0100

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,28 @@
 #include "main.h"
 
 /**
+ * add_white_spaces - adds white spaces to the standard output
+ * @n: number of white spaces
+ * Return: Always void
+ */
+void add_white_spaces(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+}
+
+/**
  * print_times_table - prints the time table of a given integer @n
  * @n: the bound to generate the time table
  * Return: Always 0
  */
 void print_times_table(int n)
 {
-	if(n <= 15 && n >= 0)
+	if (n <= 15 && n >= 0)
 	{
 		int i;
 
@@ -17,30 +32,24 @@ void print_times_table(int n)
 
 			for (j = 0; j <= n; j++)
 			{
-				int result;
+				int result = i * j;
 
-				result = i * j;
 				if (result == 0 && j == 0)
-				{
 					_putchar(result + '0');
-				}
 				else if (result < 10)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					add_white_spaces(3);
 					_putchar(result + '0');
 				}
 				else if (result >= 10 && result < 100)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					add_white_spaces(2);
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + '0');
 				}
 				else if (result >= 100)
 				{
-					_putchar(' ');
+					add_white_spaces(1);
 					_putchar(result / 100 + '0');
 					_putchar((result % 100) / 10 + '0');
 					_putchar(result % 10 + '0');

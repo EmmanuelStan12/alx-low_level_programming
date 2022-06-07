bfc5a62a18bb20bb84fea5d10f1462eac3d686a8
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-07 10:47:08 +0100

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Entry point
+ * prints the alphabets in sequence from a -> z
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c != 123)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}

8dbc66e6c10c2eee00acf277418342d31ada3b84
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-09 17:16:07 +0100

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,13 +16,9 @@ void more_numbers(void)
 			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
-			else
-				_putchar(j + '0');
+			_putchar((j % 10) + '0');
 		}
-		if (i != 9)
-			_putchar('\n');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

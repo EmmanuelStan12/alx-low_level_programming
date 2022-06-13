8abd91746fac11538f7259c4e9aaa485fbfac122
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-13 14:14:16 +0100

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints the values of a given array
+ * @n: size of the array
+ * @a: array to be evaluated
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != n - 1)
+		       printf("%d, ", *(a + i));
+		else
+			printf("%d\n", *(a + i));
+		i++;
+	}
+}

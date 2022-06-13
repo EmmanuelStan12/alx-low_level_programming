0d5580521b7612ab3e3b760d324e617b1ad6eb2d
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-13 13:46:03 +0100

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a given string
+ * @s: string to be evaluated
+ */
+void rev_string(char *s)
+{
+	int i;
+	int mid;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s[i + 1] != '\0')
+		i++;
+	mid = i / 2;
+	while (j != mid)
+	{
+		char temp = s[j];
+		*(s + j) = s[i];
+		*(s + i) = temp;
+		j++;
+		i--;
+	}
+}

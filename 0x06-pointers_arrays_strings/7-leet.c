d845223b6d06d5cd37445c1c1893348b21477725
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-15 12:39:45 +0100

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337
+ * @c: string to be encoded
+ * Return: the address of the string
+ */
+char *leet(char *c)
+{
+	int i;
+	char l[] = "aeotl";
+	int eq[] = { 4, 3, 0, 7, 1 };
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		int j;
+
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (l[j] == c[i] || c[i] == l[j] - 32)
+				*(c + i) = eq[j] + '0';
+		}
+		i++;
+	}
+	putchar('\n');
+	return (c);
+}

aef55ebe3384aa443582e1ee097e04a039e91f96
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-16 10:36:44 +0100

    More Strings, Arrays and Pointers

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -2,11 +2,26 @@
 
 int main(void)
 {
-	int i;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	printf("the integer i with address %p\n", &i);
-	printf("the array with address %p\n", a);
-	return 0;
+  a[2] = 1024;
+  a[0] = 100;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  n = 20;
+  *(p + 8) = 98;
+  printf("first value is %d\n", *(p + 5));
+  printf("or %d\n", *(p + 20));
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }

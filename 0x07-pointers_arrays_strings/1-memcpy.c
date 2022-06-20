56dfc41bd5331ef7b34cb21780cb51b5ef3bfc57
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-20 09:34:10 +0100

    task 3

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: destination of the bytes to be copied
+ * @src: the source of the bytes to be copied
+ * Return: the address of the destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string as reference
+ * @c: character to be located
+ * Return: the address of the first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p;
+
+	p = NULL;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+		i++;
+	}
+	return (p);
+}

1b3fbea74b61ea233054467773f90c071a67e4e3
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-24 11:24:52 +0100

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * @argc: the count of the arguments passed
+ * @argv: the array of the arguments passed
+ * Return: 0 (Always)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul;
+
+	i = 1;
+	mul = 1;
+	if (argc - 1 == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (i < argc)
+	{
+		mul = mul * atoi(*argv[i]);
+		i++;
+	}
+	printf("%d\n", mul);
+	return (0);
+}

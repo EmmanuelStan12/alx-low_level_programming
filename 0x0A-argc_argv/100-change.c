4c87dbbf5f9de397a5c9c126a73992f24cae3ce0
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-06-24 15:57:31 +0100

    0x0A. C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,15 +9,42 @@
  */
 int main(int argc, char *argv[])
 {
+	int value, change;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (*argv[1] == '-')
+	value = atoi(argv[1]);
+	if (value < 0)
 	{
 		printf("0\n");
 		return (1);
 	}
+	if (value % 25 >= 0)
+	{
+		change = change + (value / 25);
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		change = change + (value / 10);
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		change = change + (value / 5);
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		change = change + (value / 2);
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		change = change + value;
+	}
+	printf("%d\n", change);
 	return (0);
 }

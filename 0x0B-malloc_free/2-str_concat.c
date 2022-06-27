a9e34c9959824400caf530420354350c1ce4fa0f
CodedDeveloper117 <coded.developer098@gmail.com>
2022-06-27 13:09:40 +0100

    task 3 at Mon Jun 27 01:09:40 PM WAT 2022

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,13 +19,19 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		return (s1);
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	{
+		p = malloc(sizeof(char));
+		*p = '\0';
+		return (p);
+	}
 	while (s1[s1_size] != '\0')
 		s1_size++;
 	while (s2[s2_size] != '\0')
 		s2_size++;
 	p = (char *)malloc((s1_size + s2_size - 1) * sizeof(char));
 
+	if (p == NULL)
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		*(p + i) = s1[i];

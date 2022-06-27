7f0f8076cf9d1563dcc598a8a727867be32656a8
CodedDeveloper117 <coded.developer098@gmail.com>
2022-06-27 13:29:33 +0100

    task 2 at Mon Jun 27 01:29:33 PM WAT 2022

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,34 +14,38 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int i = 0;
 
-	if (s1 == NULL)
-		return (s2);
-	if (s2 == NULL)
-		return (s1);
 	if (s1 == NULL && s2 == NULL)
 	{
 		p = malloc(sizeof(char));
 		*p = '\0';
 		return (p);
 	}
-	while (s1[s1_size] != '\0')
-		s1_size++;
-	while (s2[s2_size] != '\0')
-		s2_size++;
-	p = (char *)malloc((s1_size + s2_size - 2) * sizeof(char));
+	if (s1 != null)
+	{
+		while (s1[s1_size] != '\0')
+			s1_size++;
+	}
+	if (s2 != null)
+	{
+		while (s2[s2_size] != '\0')
+			s2_size++;
+	}
+	p = (char *)malloc((s1_size + s2_size + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (NULL);
-	for (; *s1 != '\0'; s1++)
-	{
-		*(p + i) = *s1;
-		i++;
-	}
-	for (; *s2 != '\0'; s2++)
-	{
-		*(p + i) = *s2;
-		i++;
-	}
+	if (s1 != NULL)
+		for (; *s1 != '\0'; s1++)
+		{
+			*(p + i) = *s1;
+			i++;
+		}
+	if (s2 != NULL)
+		for (; *s2 != '\0'; s2++)
+		{
+			*(p + i) = *s2;
+			i++;
+		}
 	*(p + i) = '\0';
 	return (p);
 }

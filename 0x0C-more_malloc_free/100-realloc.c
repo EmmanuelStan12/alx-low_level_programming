c0b57c1f0fd6372ebe5e891e3427ae39c276e0cb
CodedDeveloper117 <coded.developer098@gmail.com>
2022-07-01 16:13:34 +0100

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		unsigned int i = 0;
 
 		b = (char *)ptr;
-		for (i = 0; i < new_size; i++)
+		for (i = 0; i < new_size && i < old_size; i++)
 			*(p + i) = b[i];
 		free(ptr);
 	}

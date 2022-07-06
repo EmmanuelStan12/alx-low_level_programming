ac6df61df5d905cd399578f969c4dc8d03297291
CodedDeveloper117 <coded.developer098@gmail.com>
2022-07-06 13:18:22 +0100

    task 100 at Wed Jul  6 01:18:22 PM WAT 2022

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: no of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int arg;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	arg = atoi(argv[1]);
+	if (arg < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}

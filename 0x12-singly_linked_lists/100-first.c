79b62b97fad0ce3760ad7540177aa259a0a1956a
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-07-14 15:47:51 +0100

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void start_up(void) __attribute__ ((constructor));
+
+/**
+ * start_up - runs before the main function
+ *
+ * Return: void
+ */
+void start_up(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

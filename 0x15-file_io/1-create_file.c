4b6bd1a4e2203669489ce4636bd188945b430c01
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-07-25 14:43:05 +0100

    Fixed Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	content = text_content;
 	size = 0;
 	while (*content != '\0' && content != NULL)

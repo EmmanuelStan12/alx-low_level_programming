c88fb084aaecf5dd46c664eb8e80a56ed0616672
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-07-25 14:39:06 +0100

    Fixed Betty Issures

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
+/**
+ * read_textfile - read a text file
+ * @filename: the name of the file
+ * @letters: the number to be copied
+ * Return: the size of the buffer written
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_d;
 	char *buf;
 	ssize_t rcount, wcount;
 
-	file_d = open(filename, O_RDONLY);
+	file_d = open(filename, O_RDWR);
 
 	if (file_d == -1)
-		exit(1);
+		return (0);
 	buf = malloc(letters * sizeof(*buf));
 	if (buf == NULL)
 		return (0);

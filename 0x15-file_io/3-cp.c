e8ed0bb10db234e2f9b8d411259898c25499fbad
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-07-25 20:19:58 +0100

    Fixed Betty issues

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,5 @@
 #include "main.h"
-
-/**
- * file_exists - check if a file exists and can be read
- * @file: file to be evaluated
- * Return: 1 if true or 0 if false
- */
-int file_exists(char *file)
-{
-	if (!access(file, F_OK) && !access(file, R_OK))
-		return (1);
-	return (0);
-}
+#define SE STDERR_FILENO
 
 /**
  * main - Entry point
@@ -26,50 +15,27 @@ int main(int argc, char **argv)
 	mode_t mode;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = argv[argc - 2];
 	i_file = open(file_from, O_RDONLY);
-	if (!file_exists(file_from) || i_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+	if (i_file == -1)
+		dprintf(SE, "Error: Can't read from file %s\n", file_from), exit(98);
 	file_to = argv[argc - 1];
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	o_file = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (o_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	do
-	{
+		dprintf(SE, "Error: Can't write to %s\n", file_to), exit(99);
+	do {
 		sz = read(i_file, buffer, sizeof(buffer));
 		if (sz == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			dprintf(SE, "Error: Can't read from file %s\n", file_from), exit(98);
 		wstatus = write(o_file, buffer, sz);
 		if (wstatus == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-	while (sz > 0);
+			dprintf(SE, "Error: Can't write to %s\n", file_to), exit(99);
+	} while (sz > 0);
 	if (close(i_file) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", i_file);
-		exit(100);
-	}
+		dprintf(SE, "Error: Can't close fd %d", i_file), exit(100);
 	if (close(o_file) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", o_file);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", o_file), exit(100);
 	return (0);
 }

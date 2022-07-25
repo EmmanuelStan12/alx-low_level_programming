d406d78d4077d494218895613e50adf9b0f7a768
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-07-25 14:49:07 +0100

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,17 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	content = text_content;
-	size = 0;
-	while (*content != '\0' && content != NULL)
-	{
-		size++;
-		content++;
-	}
 	if (file_d == -1)
 		return (-1);
 	if (text_content)
 	{
+		size = 0;
+		content = text_content;
+		while (*content != '\0')
+		{
+			size++;
+			content++;
+		}
 		status = write(file_d, text_content, size);
 		if (status == -1)
 			return (-1);

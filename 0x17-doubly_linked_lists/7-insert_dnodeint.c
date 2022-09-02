a73cf074b87cc2fa37b0677fffe1000ff76b83a3
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-02 13:01:54 +0100

    task: Insert at index
    Wrote a function that inserts a new node at a given position.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a node at a given index
+ * @head: the pointer to the first element in the list
+ * @idx: the index of the place to be inserted
+ * @n: the number to be inserted
+ * Return: the address of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+{
+	dlistint_t *current, *temp, *next;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(*temp));
+	if (temp == NULL)
+		return (NULL);
+	current = *head;
+	if (idx == 0)
+	{
+		temp->n = n;
+		temp->next = current;
+		current->prev = temp;
+		*head = temp;
+		return (temp);
+	}
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (current->next == NULL)
+			return (NULL);
+		current = current->next;
+	}
+	temp->n = n;
+	next = current->next;
+	current->next = temp;
+	temp->prev = current;
+	temp->next = next;
+	next->prev = temp;
+	return (temp);
+}

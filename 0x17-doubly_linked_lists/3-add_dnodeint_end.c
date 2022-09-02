fc9e2b96a3869a828def69ae9399bbd2bd97d0bd
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-02 10:36:21 +0100

    task: Add node at the end
    Wrote a function that adds a new node at the end of a linked list.

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node to the start of the list
+ * @head: head of the list
+ * @n: string to be copied
+ * Return: the pointer to the first element
+ */
+list_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+	dlistint_t *h = *head;
+
+	temp = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->next = NULL;
+	temp->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (*head);
+	}
+	while (h->next != NULL)
+		h = h->next;
+	temp->prev = h;
+	h->next = temp;
+	return (temp);
+}
